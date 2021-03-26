f65163e0d306bff5305edc7cfe3d898d619d893c
JZJisawesome <JZJisawesome123@hotmail.com>
2021-03-26 13:50:26 -0400

    Add license

--- src/cpu/cpu.c
+++ src/cpu/cpu.c
@@ -1,5 +1,8 @@
  
 void main()
 {
+    //First thing to print should be "CPU MCU Initialized" (at 25, 32), then # of free basic bytes (at 1, 40)
+    
+    
     return;//Temporary
 }
--- src/video/video.c
+++ src/video/video.c
@@ -9,9 +9,17 @@ void main()
 {
     SR_setCharacterRom(vincentFont);
     SR_setFrameBuffer((uint8_t*)ramFB);
+    
+    SR_drawText(1, 8,  "JZJBasic on Dual STM32F103C8T6's");
+    SR_drawText(1, 16, "Copyright (c) 2021 John Jekel");
+    SR_drawText(1, 24, "MIT Licensed: https://gitlab.com/JZJisawesome/stm32basic");
+    
     Composite_init((uint8_t*)ramFB);
     
-    SR_drawText(1, 8, "Hello World!");
+    //Do init code here
+    
+    SR_drawText(1, 32, "Video MCU Initialized : ");
+    
     
     //TODO handle commands from second spi peripheral
     
