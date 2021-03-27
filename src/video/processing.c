4a21cabdd809b4fe916bfd05a258d293e1668a46
JZJisawesome <JZJisawesome123@hotmail.com>
2021-03-27 08:32:19 -0400

    Work on special character handling

--- src/video/processing.c
+++ src/video/processing.c
@@ -5,7 +5,7 @@
 #include "spibus.h"
 
 static fifo_t commandQueue;
-static uint_fast16_t xPosition = 200;//Pixels
+static uint_fast16_t xPosition = 208;//Pixels
 static uint_fast16_t yPosition = 32;//Pixels
 
 static void processingLoop();
@@ -60,28 +60,69 @@ static void processingLoop()
             }
             else//Character write command
             {
+                //NOTE: Command bits [14:7] may have additional info that can be used
+                
                 char character = command & 0x7F;
                 switch (character)
                 {
-                    //TODO ensure the special cases work correctly
-                    case '\t':
+                    //TODO ensure the special cases work correctly and have proper bounds checking
+                    case 0x01://Move up
+                    {
+                        yPosition -= 8;
+                        break;
+                    }
+                    case 0x02://Move left
+                    {
+                        xPosition -= 8;
+                        break;
+                    }
+                    case 0x03://Move down
+                    {
+                        yPosition += 8;
+                        break;
+                    }
+                    case 0x04://Move right
+                    {
+                        xPosition += 8;
+                        break;
+                    }
+                    case 0x08://Backspace
+                    {//TODO fix this
+                        xPosition -= 8;
+                        SR_drawCharByByte(xPosition / 8, yPosition, ' ');
+                        break;
+                    }
+                    case '\t'://Tab
                     {
                         xPosition += 3 * 8;//3 spaces
                         incrementCharacterPosition();//Handle fourth space with increment code to deal with wrapping
                         break;
                     }
-                    case '\n':
-                    case '\r':
+                    case '\n'://New line
+                    case '\r'://Carriage return
                     {
                         xPosition = 8;
                     }//Fallthrough
-                    case '\v':
+                    case '\v'://Vertical tab
                     {
                         yPosition += 8;
                         
                         if (yPosition >= PROCESSING_LINES)
                             yPosition = 8;//So character is not cut off
                         
+                        break;
+                    }
+                    case 0x0C://Form feed
+                    {
+                        //commented out for testing
+                        //xPosition = 8;
+                        //yPosition = 8;
+                        break;
+                    }
+                    case 0x7F://Delete
+                    {//TODO fix this
+                        xPosition += 8;
+                        SR_drawCharByByte(xPosition / 8, yPosition, ' ');
                         break;
                     }
                     default:
@@ -97,7 +138,7 @@ static void processingLoop()
     }
 }
 
-void incrementCharacterPosition()
+static void incrementCharacterPosition()
 {
     xPosition += 8;
     
